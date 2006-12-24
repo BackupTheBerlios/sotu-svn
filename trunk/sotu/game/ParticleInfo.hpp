6e966d435102fbd9f5bc82dc688a449c8c5540cc
mbabuskov <mbabuskov@5084d6f2-cfd4-0310-be57-a53b2e5741cc>
2006-12-24 22:47:51 +0000

    copyright

--- trunk/sotu/game/ParticleInfo.hpp
+++ trunk/sotu/game/ParticleInfo.hpp
@@ -2,6 +2,7 @@
 //   Structure to keep particle information.
 //
 // Copyright (C) 2001 Frank Becker
+// Copyright (C) 2006 Milan Babuskov
 //
 // This program is free software; you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
